73054692c16f0236fac4015f1eddffdd22e14861
Puneet Shankar <puneetshankar2021@gmail.com>
2026-07-15 02:24:02 +0530

    Time: 0 ms (100%), Space: 22.4 MB (98.22%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        return nums.empty() ? 0 : std::distance(nums.begin(), std::unique(nums.begin(), nums.end()));
+        int j=0;
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            if(nums[i]!=nums[j]){
+                j++;
+                nums[j]=nums[i];
+            }
+        }
+        return j+1;
     }
 };
