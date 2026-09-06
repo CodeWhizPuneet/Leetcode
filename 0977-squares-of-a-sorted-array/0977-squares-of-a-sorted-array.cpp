94b8487b1e4dd118c0cffd6e8246dc4bd9195975
Puneet Shankar <puneetshankar2021@gmail.com>
2026-09-07 01:27:02 +0530

    Time: 2 ms (55.41%), Space: 30.1 MB (99.01%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,10 +1,40 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i = 0;i<nums.size();i++){
-            nums[i]=nums[i]*nums[i];
+        int n = nums.size();
+        
+        // 1. Square all elements in-place and find split point
+        int count = -1; // Pointer for negative numbers
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0) {
+                count = i;
+            }
+            nums[i] = nums[i] * nums[i];
         }
-        sort(nums.begin(),nums.end());
-        return {nums};
+
+        vector<int> res(n);
+        int idx = 0;
+        int p = count + 1; // Pointer for non-negative numbers
+
+        // 2. Merge two halves outward
+        while (count >= 0 && p < n) {
+            if (nums[count] <= nums[p]) {
+                res[idx++] = nums[count--];
+            } else {
+                res[idx++] = nums[p++];
+            }
+        }
+
+        // 3. Collect remaining elements from negative side
+        while (count >= 0) {
+            res[idx++] = nums[count--];
+        }
+
+        // 4. Collect remaining elements from positive side
+        while (p < n) {
+            res[idx++] = nums[p++];
+        }
+
+        return res;
     }
 };
