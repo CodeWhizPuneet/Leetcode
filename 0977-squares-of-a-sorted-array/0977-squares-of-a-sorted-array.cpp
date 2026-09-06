3dc47db2ae61ddccdcf4ac9addc69cf65d824c9f
Puneet Shankar <puneetshankar2021@gmail.com>
2026-09-07 02:02:18 +0530

    Time: 0 ms (100%), Space: 32.2 MB (5.01%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,21 +1,65 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> res(n);
-        int left = 0, right = n - 1;
-
-        // Fill result from back to front with the larger square
-        for (int i = n - 1; i >= 0; i--) {
-            if (abs(nums[left]) > abs(nums[right])) {
-                res[i] = nums[left] * nums[left];
-                left++;
-            } else {
-                res[i] = nums[right] * nums[right];
-                right--;
+        vector<int> a;
+        vector<int> b;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<0){
+                a.push_back(nums[i]);
+            }
+            else{
+                b.push_back(nums[i]);
             }
         }
+        if(a.size()==0){
+            for(int i=0;i<nums.size();i++){
+                nums[i]=nums[i]*nums[i];
+            }
+            return nums;
+        }
+        else if(b.size()==0){
+            for(int i=0;i<nums.size();i++){
+                nums[i]=nums[i]*nums[i];
+            }
+            reverse(nums.begin(), nums.end());
+            return nums;
+        }
+        int i = 0;
+        int j = 0;
+        int idx = 0;
+        int n = a.size();
+        int m = b.size();
+        vector<int> res(m+n);
+        for(int i=0;i<n;i++){
+            a[i]=a[i]*a[i];
+        }
+        reverse(a.begin(),a.end());
 
+        for(int j=0;j<m;j++){
+            b[j]=b[j]*b[j];
+        }
+        while(i<n and j<m){
+            if(a[i]<=b[j]){
+                res[idx]=a[i];
+                i++;
+                idx++;
+            }
+            else{
+                res[idx]=b[j];
+                j++;
+                idx++;
+            }
+        }
+        while(i<n){
+            res[idx]=a[i];
+            i++;
+            idx++;
+        }
+        while(j<m){
+            res[idx]=b[j];
+            j++;
+            idx++;
+        }
         return res;
     }
 };
