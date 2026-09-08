090c5954d620d9e6b78647d1b69d0ebef4a87b9c
Puneet Shankar <puneetshankar2021@gmail.com>
2026-09-09 02:13:40 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.72%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int res = 0;
-        if(n >=1000){
-            res += n - 1000+1;
+        // int res = 0;
+        // if(n >=1000){
+        //     res += n - 1000+1;
 
-        }
-        else if(n>=1000000){
-            res = res + (n-1000000 + 1);
-        }
-        else{
-            return res;
+        // }
+        // else if(n>=1000000){
+        //     res = res + (n-1000000 + 1);
+        // }
+        // else{
+        //     return res;
+        // }
+        // return res;
+        int res = 0;
+        int new_comma = 1000;
+        while(n>=new_comma){
+            res = res + (n-new_comma + 1);
+            new_comma = new_comma*1000;
         }
         return res;
     }
