1e60bf4b66c70b5321fb5fbc73ed7747d51fce23
Puneet Shankar <puneetshankar2021@gmail.com>
2026-09-09 00:31:22 +0530

    Time: 0 ms (100%), Space: 11.7 MB (48.85%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,31 +1,48 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int one =0;
-        int two =0;
-        int three = 0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                one++;
+        int left = 0;
+        int mid = 0;
+        int right = nums.size()-1;
+        while(mid<=right){
+            if(nums[mid]==0){
+                swap(nums[left],nums[mid]);
+                left++;
+                mid++;
             }
-            else if(nums[i]==1){
-                two++;
-            }
-            else{
-                three++;
-            }
-        }
-        for(int i=0;i<nums.size();i++){
-            if(i<one){
-                nums[i]=0;
-            }
-            else if(i<one+two){
-                nums[i]=1;
+            else if(nums[mid]==2){
+                swap(nums[mid],nums[right]);
+                right--;
             }
             else{
-                nums[i]=2;
+                mid++;
             }
         }
+        // int one =0;
+        // int two =0;
+        // int three = 0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==0){
+        //         one++;
+        //     }
+        //     else if(nums[i]==1){
+        //         two++;
+        //     }
+        //     else{
+        //         three++;
+        //     }
+        // }
+        // for(int i=0;i<nums.size();i++){
+        //     if(i<one){
+        //         nums[i]=0;
+        //     }
+        //     else if(i<one+two){
+        //         nums[i]=1;
+        //     }
+        //     else{
+        //         nums[i]=2;
+        //     }
+        // }
 
     }
 };
