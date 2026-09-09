94f419bd2c66a5538e4c3f20ec24dda11c04dc22
Puneet Shankar <puneetshankar2021@gmail.com>
2026-09-09 13:12:46 +0530

    Time: 0 ms (100%), Space: 9.1 MB (57.33%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -3,14 +3,8 @@ public:
     long long countCommas(long long n) {
         long long new_comma = 1000;
         long long res = 0;
-        if(n<1000){
-            return 0;
-        }
         while(n>=new_comma){
             res = res + (n-new_comma+1);
-            // if(new_comma>10^18){
-            //     break;
-            // }
             new_comma = new_comma * 1000;
         }
         return res;
