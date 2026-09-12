866e054ca59b17a344eb47d0c4dc23588ec549f5
Puneet Shankar <puneetshankar2021@gmail.com>
2026-09-13 02:42:36 +0530

    Time: 84 ms (19.39%), Space: 113.5 MB (11.25%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> hash;
+        for(int i = 0;i<n;i++){
+            hash[nums[i]] = hash[nums[i]] + 1;
+        }
+        for(auto x : hash){
+            if(x.second > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
