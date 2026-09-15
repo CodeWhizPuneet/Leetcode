dbd01be08819474832d83d6c5c9c00146fad29e3
Puneet Shankar <puneetshankar2021@gmail.com>
2026-09-16 02:45:07 +0530

    Time: 11 ms (73.74%), Space: 18.4 MB (74.07%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         vector<int> lastPos(128, -1);
-        int maxLength = 0;
+        int maxxLength = 0;
         int left = 0;
 
         for (int right = 0; right < s.length(); ++right) {
@@ -15,9 +15,9 @@ public:
 
             // Update character's latest position and calculate window size
             lastPos[currentChar] = right;
-            maxLength = max(maxLength, right - left + 1);
+            maxxLength = max(maxxLength, right - left + 1);
         }
 
-        return maxLength;
+        return maxxLength;
     }
 };
